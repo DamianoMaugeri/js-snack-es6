a8fee5ac7cde81eb97e55f47a7e1c3c569267fd5
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-25 18:20:53 +0200

    [fix] array snack 1

--- snack 1/script.js
+++ snack 1/script.js
@@ -1,63 +1,63 @@
 console.log('prova snack 1')
 
-const arraybici = []
+const arraybici = [
 
-const bici1 = {
-    nome : 'Giant Propel Advanced SL 0',
-    peso : 6.8,
-}
+    {
+        nome: 'Giant Propel Advanced SL 0',
+        peso: 6.8,
+    },
 
-const bici2 = {
-    nome : 'Scott Addict RC Ultimate',
-    peso : 6.7,
-}
+    {
+        nome: 'Scott Addict RC Ultimate',
+        peso: 6.7,
+    },
 
-const bici3 = {
-    nome : 'Specialized S-Works Aethos/Sram Red Etap AXS',
-    peso : 6.36,
-}
+    {
+        nome: 'Specialized S-Works Aethos/Sram Red Etap AXS',
+        peso: 6.36,
+    },
 
-const bici4 = {
-    nome : 'Wilier Zero SLR',
-    peso : 6.8,
-}
+    {
+        nome: 'Wilier Zero SLR',
+        peso: 6.8,
+    },
 
-const bici5 = {
-    nome : 'Orbea Orca M10iLTD PWR',
-    peso : 6.64,
-}
+    {
+        nome: 'Orbea Orca M10iLTD PWR',
+        peso: 6.64,
+    },
 
-const bici6 = {
-    nome : 'Look 795 Blade RS',
-    peso : 7,
-}
+    {
+        nome: 'Look 795 Blade RS',
+        peso: 7,
+    },
 
-const bici7 = {
-    nome : 'Cube Litening Air C:68X SLT',
-    peso : 6.6,
-}
+    {
+        nome: 'Cube Litening Air C:68X SLT',
+        peso: 6.6,
+    },
 
-const bici8 = {
-    nome : 'Bianchi Specialissima RC/Shimano Dura-Ace',
-    peso : 6.6,
-}
+    {
+        nome: 'Bianchi Specialissima RC/Shimano Dura-Ace',
+        peso: 6.6,
+    }
+]
 
-arraybici.push(bici1,bici2,bici3,bici4,bici5,bici6,bici7,bici8)
-console.log(arraybici)
+
 
 let oggettoBiciLeggera = arraybici[0]
 
 
 
-for (let i=0; i < arraybici.length; i++){
-    
+for (let i = 0; i < arraybici.length; i++) {
 
-    if (arraybici[i].peso < oggettoBiciLeggera.peso){
+
+    if (arraybici[i].peso < oggettoBiciLeggera.peso) {
 
         oggettoBiciLeggera = arraybici[i];
 
     }
-    
+
 }
 
 console.log(oggettoBiciLeggera)
