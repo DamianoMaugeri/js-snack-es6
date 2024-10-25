1bf834371622d26e208857d4b1af9a5a0e1dfb7c
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-25 18:07:55 +0200

    [add] snack 2

---
+++ snack 2/script.js
@@ -0,0 +1,68 @@
+console.log('prova snack 2')
+
+const squadre = [
+
+    {
+        nome : 'juve',
+        punti : 0,
+        'falli subiti':0
+    },
+
+    {
+        nome : 'milan',
+        punti : 0,
+        'falli subiti':0
+    },
+
+    {
+        nome : 'inter',
+        punti : 0,
+        'falli subiti':0
+    },
+
+    {
+        nome : 'napoli',
+        punti : 0,
+        'falli subiti':0
+    },
+
+    {
+        nome : 'fiorentina',
+        punti : 0,
+        'falli subiti':0
+    }
+]
+//console.log(squadre)
+
+const arrayNomiFalli = []
+
+
+
+for (let i = 0; i < squadre.length; i++){
+    squadre[i].punti = randomIntFromMinToMax(5,100);
+    squadre[i]['falli subiti'] = randomIntFromMinToMax(2,30);
+
+    let newobj = {}
+
+    newobj.nome = squadre[i].nome
+    newobj['falli subiti'] = squadre[i]['falli subiti']
+
+    arrayNomiFalli.push(newobj)
+    
+
+
+}
+//console.log(squadre)
+
+console.log(arrayNomiFalli)
+
+
+
+////
+
+
+function randomIntFromMinToMax(min,max) { 
+     const randomInt = Math.floor(Math.random() * (max - min+ 1)) + min;
+
+     return randomInt
+ }
